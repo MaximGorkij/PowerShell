579365f1db58cbc7bbe67a36fd9bd28e3656732f
Maxim Gorkij <maxim.gorkij@gmail.com>
2025-07-22 11:10:00 +0200

    doplnenie zabbix

--- Intune/zabbix/install.ps1
+++ Intune/zabbix/install.ps1
@@ -1,4 +1,4 @@
-msiexec /i "zabbix_agent2.msi" /qn
+msiexec /i "zabbix_agent2_plugins-7.4.0.msi" /qn
 Start-Sleep -Seconds 5
 Copy-Item "zabbix_agent2.conf" "C:\Program Files\Zabbix Agent 2\" -Force
 certutil -f -p ZabbixSecret123 -importpfx "agent.pfx"
